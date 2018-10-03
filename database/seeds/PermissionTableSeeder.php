17765a575d2ec332ebe101a0e2e75d7a8110e0b3
nitin-shendkar <nitin.shendkar@olx.co.id>
2018-10-03 22:39:22 +0530

    laravel 5.7

---
+++ database/seeds/PermissionTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Permission;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = [
+            'role-list',
+            'role-create',
+            'role-edit',
+            'role-delete',
+            'product-list',
+            'product-create',
+            'product-edit',
+            'product-delete'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission,'guard_name'  => $permission]);
+        }
+    }
+}
